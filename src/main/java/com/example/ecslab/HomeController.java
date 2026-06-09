5987b966018c00126d98b1b76e37c569d27e2306
alfreddey <lilscar246@gmail.com>
2026-06-09 11:35:26 +0000

    update text in controller to test workflow

--- src/main/java/com/example/ecslab/HomeController.java
+++ src/main/java/com/example/ecslab/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    private static final String FULL_NAME = "Alfred Dey";
-    private static final String LAB_NAME = "AWS ECS Lab";
+    private static final String FULL_NAME = "Alfred Nelly Dey";
+    private static final String LAB_NAME = "AWS ECS Lab V2";
 
     @GetMapping("/")
     public String home(Model model) {
